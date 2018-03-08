6eb81a7c80cb857b2d6bd62c0e9a0a6cb0822d90
Felix Hillingshaeuser <felix@mxcore.de>
2018-03-08 21:58:18 +0100

    [root] add configuration checks

--- cmd/root.go
+++ cmd/root.go
@@ -28,6 +28,7 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.org/x/sys/unix"
 )
 
 var cfgFile string
@@ -55,6 +56,7 @@ func Execute() {
 }
 
 func init() {
+	// read configuration
 	cobra.OnInitialize(initConfig)
 
 	// Flag for config file
@@ -97,5 +99,65 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
+	// validate configuration
+
+	// store configurations in a variables, as we use them quite often
+	repoPath := viper.GetString("yum.repopath")
+	createrepoBinary := viper.GetString("yum.createrepoBinary")
+	rpmBinary := viper.GetString("yum.rpmBinary")
+	helpFile := viper.GetString("yum.helpFile")
+
+	// check if repo path exists
+	info, err := os.Stat(repoPath)
+	if err != nil {
+		fmt.Printf("configured repo path '%s' does not exists\n", repoPath)
+		os.Exit(1)
+	}
+
+	// check if repo path is a directory
+	if !info.IsDir() {
+		fmt.Printf("configured repo path '%s' is not a directory\n", repoPath)
+		os.Exit(1)
+	}
+
+	// check if repo path is writeable
+	if unix.Access(repoPath, unix.W_OK) != nil {
+		fmt.Printf("configured repo path '%s' is not writeable\n", repoPath)
+		os.Exit(1)
+	}
+
+	// check if createrepo binary exists
+	info, err = os.Stat(createrepoBinary)
+	if err != nil {
+		fmt.Printf("configured createrepo binary '%s' does not exists\n", createrepoBinary)
+		os.Exit(1)
+	}
+
+	// check if createrepo binary is executable
+	if unix.Access(createrepoBinary, unix.X_OK) != nil {
+		fmt.Printf("configured createrepo binary '%s' is not executable\n", createrepoBinary)
+		os.Exit(1)
+	}
+
+	// check if rpm binary exists
+	info, err = os.Stat(rpmBinary)
+	if err != nil {
+		fmt.Printf("configured rpm binary '%s' does not exists\n", rpmBinary)
+		os.Exit(1)
+	}
+
+	// check if rpm binary is executable
+	if unix.Access(rpmBinary, unix.X_OK) != nil {
+		fmt.Printf("configured rpm binary '%s' is not executable\n", rpmBinary)
+		os.Exit(1)
+	}
+
+	// check if help file exists
+	info, err = os.Stat(helpFile)
+	if err != nil {
+		fmt.Printf("configured help file '%s' does not exists\n", helpFile)
+		os.Exit(1)
 	}
 }
